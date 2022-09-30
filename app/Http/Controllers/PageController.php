96130fd58f1eb186349e6d9d5499ffa8dbf9bd6f
ariannadepasquale <depasqualearianna@gmail.com>
2022-09-30 10:51:08 +0200

    Appunti-correzioni- cose da testare

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,12 +10,18 @@ class PageController extends Controller
 {
     public function index(){
         
-        $trains = Train::all();        
+        // $trains = Train::all();        
         // var_dump($trains);
         // die();
 
         $train = Train::all()->where('departure_date', '2022-09-29');
         return view('homepage', ['trains'=> $train]);
+
+
+        // $train = Train::whereDate()->where('departure_date', '2022-09-29')->get;
+        
+        return view('homepage', ['trains'=> $train]); 
+        // compact("trains")
         
         
         // return view('homepage');   
--- database/seeds/FakerTrainSeeder.php
+++ database/seeds/FakerTrainSeeder.php
@@ -16,15 +16,16 @@ class FakerTrainSeeder extends Seeder
             $newTrain = new Train();
             
             $newTrain->agency=$faker->randomElement(['Trenitalia', 'Italo','Air']);
-            $newTrain->departure_station= $faker->city();
-            $newTrain->arrival_station= $faker->city();
-            $newTrain->departure_date= $faker->date();
-            $newTrain->departure_time= $faker->time();
-            $newTrain->arrival_time= $faker->time();
-            $newTrain->train_code= $faker->numberBetween(1000, 3000);
-            $newTrain->carriage_number=$faker->numberBetween(1, 10);
-            $newTrain->in_time=$faker->randomElement(['yes', 'no']);
-            $newTrain->cancelled=$faker->randomElement(['yes', 'no']);
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            // $newTrain->departure_date= $faker->date();
+            $newTrain->departure_date = $faker->dateTimeBetween('-3 days', '+3 days');
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->numberBetween(1000, 3000);
+            $newTrain->carriage_number = $faker->numberBetween(1, 10);
+            $newTrain->in_time = $faker->randomElement(['yes', 'no']);
+            $newTrain->cancelled = $faker->randomElement(['yes', 'no']);
             $newTrain->save();
         }
     }
